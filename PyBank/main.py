19d61dd3872d1dcb9995eb0377a16845f598d2fc
tat3446 <127369366+tat3446@users.noreply.github.com>
2023-05-29 20:46:24 -0500

    adding PyBank files and readme

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,39 +1,79 @@
 # Modules
 import os
 import csv
+import sys
 
-date = []
+prof_loss_chg = []
+prof_loss_list = []
 
 #Set path for file
-bank_csv = os.path.join("Resources", "budget_data.csv")
+bank_csv = os.path.join("Resources","budget_data.csv")
 
 #Set variables
-#month_total = 0
-#Prof_Loss_total = 0
-#prof_loss_chg = 0
+prof_loss_total = 0
+month_total = 0
+prior_profloss = 0.00
+prof_loss_avg = 0.00
+max_change = 0
+max_month = ""
+min_change = 0
+min_month = ""
 
-#prof_loss = []
-#prof_loss_chg = []
-#Prof_Loss_total = []
-
-#Read the csv file
+# #Read the csv file
 with open(bank_csv) as bank_list:
-
+    
     #CSV reader to translate contents of file
     csvreader = csv.reader(bank_list)
-
+     
     #Read in header of file
     csvheader = next(csvreader)
-    
-    #Read in data rows
+
+    #Loop through list
     for row in csvreader:
-        if row[2] not in date:
-            date.append(row[2])
 
-print(date)
+        #save date and amount
+        date = row[0]
+        amount = int(row[1])
 
+        month_total += 1
+        prof_loss_total += amount
+        
+        #Calculate change in Profit/Losses
+        if month_total > 1:
 
+            prof_loss_chg = amount - prior_profloss
+            prof_loss_list.append(prof_loss_chg)
 
+            if prof_loss_chg > max_change:
+                max_change = prof_loss_chg
+                max_month = date
+                
+            if prof_loss_chg < min_change:
+                min_change = prof_loss_chg
+                min_month = date
 
+        prior_profloss = amount
 
+    prof_loss_avg = sum(prof_loss_list) / (month_total-1)
 
+#Print statements
+
+print("Financial Analysis")
+print("----------------------------")
+print(f"Total Months: {month_total}")
+print(f"Total: ${prof_loss_total}")
+print(f"Average Change: ${prof_loss_avg:.2f}")
+print(f"Greatest Increase in Profits: {max_month} (${max_change})")
+print(f"Greatest Decrease in Profits: {min_month} (${min_change})")
+
+#Export a text file with analysis results
+
+output_path = os.path.join("analysis","PyBank.txt")
+with open(output_path, "w", encoding="utf-8") as txtfile:
+    txtfile.write("Financial Analysis\n")
+    txtfile.write("----------------------------\n")
+    txtfile.write(f"Total Months: {month_total}\n")
+    txtfile.write(f"Total: ${prof_loss_total}\n")
+    txtfile.write(f"Average Change: ${prof_loss_avg:.2f}\n")
+    txtfile.write(f"Greatest Increase in Profits: {max_month} (${max_change})\n")
+    txtfile.write(f"Greatest Decrease in Profits: {min_month} (${min_change})\n")
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # python-challenge
+
+5-29-23
+I had worked on this assignment a few weeks back.  Reviewed in office hours few times and covered the issue I was having with looping in tutor session at that time.
+Stopped working on this one and moved ahead to complete next module challenge. 
+Today finished with PyBank and reviewed three outstanding items with tutor today that I was not finding correct solutions in research.
+
+
+# Writing text file , used sites
+https://www.scaler.com/topics/python-write-list-to-file/
+https://www.pythontutorial.net/python-basics/python-write-text-file/
+
+# Formatting float for avg change
+https://java2blog.com/format-a-float-to-two-decimal-places/#:~:text=An%20f%2Dstring%20is%20a,up%20to%20two%20decimal%20places.
+
+
+
